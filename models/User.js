49a932aed215a84d35d9a62d6abf86a64b87afc4
apyt6666-art <apyt6666@gmail.com>
2026-05-30 06:24:17 +0300

    Delete models directory

--- models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = mongoose.model('User', new mongoose.Schema({
-  userId: String,
-  guildId: String,
-  messages: { type: Number, default: 0 },
-  completed: { type: Boolean, default: false }
-}));
